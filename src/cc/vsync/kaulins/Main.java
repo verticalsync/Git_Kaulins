e39424ad0d92b1ee3f77b9051460fcda8f6bf39f
verticalsync <vsync@riseup.net>
2026-09-07 10:01:20 +0300

    feat: add for-loop

--- src/cc/vsync/kaulins/Main.java
+++ src/cc/vsync/kaulins/Main.java
@@ -3,14 +3,16 @@ package cc.vsync.kaulins;
 import java.util.Random;
 
 public class Main {
-	static void mestKaulinu() {
+	static void mestKaulinu(int reizes) {
 		int skaitlis;
 		Random rand = new Random();
-		skaitlis = rand.nextInt(6) + 1;
-		System.out.printf("Uzkrita skaitlis: %d\n", skaitlis);
+		for (int i = 0; i < reizes; i++) {
+			skaitlis = rand.nextInt(6) + 1;
+			System.out.printf("Uzkrita skaitlis: %d\n", skaitlis);
+		}
 	}
 	
 	public static void main(String[] args) {
-		mestKaulinu();
+		mestKaulinu(5);
 	}
 }
