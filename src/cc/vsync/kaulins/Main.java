48c8c6ad05bb92483afc0ef425b8994ea55068b4
verticalsync <vsync@riseup.net>
2026-09-07 09:55:31 +0300

    feat: move main logic into separate function

--- src/cc/vsync/kaulins/Main.java
+++ src/cc/vsync/kaulins/Main.java
@@ -3,10 +3,14 @@ package cc.vsync.kaulins;
 import java.util.Random;
 
 public class Main {
-	public static void main(String[] args) {
+	static void mestKaulinu() {
 		int skaitlis;
 		Random rand = new Random();
 		skaitlis = rand.nextInt(6) + 1;
 		System.out.printf("Uzkrita skaitlis: %d\n", skaitlis);
+	}
+	
+	public static void main(String[] args) {
+		mestKaulinu();
 	}
 }
