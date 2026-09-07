1ac14c4fca4e5c433a4c70a8b031b765fb4644c2
verticalsync <vsync@riseup.net>
2026-09-07 10:07:10 +0300

    feat: input cycle count for for-loop

--- src/cc/vsync/kaulins/Main.java
+++ src/cc/vsync/kaulins/Main.java
@@ -1,12 +1,19 @@
 package cc.vsync.kaulins;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Cik reizes mest kauliņu? ");
+		int reizes = sc.nextInt();
+		
 		int skaitlis;
 		Random rand = new Random();
-		skaitlis = rand.nextInt(6) + 1;
-		System.out.printf("Uzkrita skaitlis: %d\n", skaitlis);
+		for (int i = 0; i < reizes; i++) {
+			skaitlis = rand.nextInt(6) + 1;
+			System.out.printf("Uzkrita skaitlis: %d\n", skaitlis);
+		}
 	}
 }
