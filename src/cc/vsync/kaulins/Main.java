940db2a7797779c4b455bde27bafab418ec8eefa
verticalsync <vsync@riseup.net>
2026-09-16 14:25:36 +0300

    feat: output highest dice roll

--- src/cc/vsync/kaulins/Main.java
+++ src/cc/vsync/kaulins/Main.java
@@ -4,20 +4,23 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-	static int mestKaulinu(int reizes) {
+	static void mestKaulinu(int reizes) {
 		int skaitlis;
 		Random rand = new Random();
 		
 		int min = 6;
+		int max = 0;
 		
 		for (int i = 0; i < reizes; i++) {
 			skaitlis = rand.nextInt(6) + 1;
 			System.out.printf("Uzkrita skaitlis: %d\n", skaitlis);
 			if (skaitlis < min)
 				min = skaitlis;
+			if (skaitlis > max)
+				max = skaitlis;
 		}
 		
-		return min;
+		System.out.printf("Mazākais skaitlis no visiem metieniem: %d\nLielākais skaitlis no visiem metiniem: %d\n", min, max);
 	}
 	
 	public static void main(String[] args) {
@@ -25,8 +28,7 @@ public class Main {
 		
 		System.out.println("Cik reizes mest kauliņu?");
 		int reizes = sc.nextInt();
-		
-		System.out.printf("Mazākais skaitlis no visiem metieniem: %s\n".formatted(mestKaulinu(reizes)));
+		mestKaulinu(reizes);
 
 		sc.close();
 	}
